923b7ea9654abb74414a3c98be4ec9f9d837cb7e
Daniel Mateos <daniel@mateos.cc>
2015-03-12 00:04:58 +1030

    now supports regexp in regexp transformer

--- lib/plumber/transform_regexp_replace.rb
+++ lib/plumber/transform_regexp_replace.rb
@@ -2,7 +2,7 @@ module Plumber
   class TransformRegexpReplace
     def transform!(flow, parameters)
       s1, s2 = parameters.split("/")
-      flow.data = flow.data.gsub(s1, s2)
+      flow.data = flow.data.gsub(Regexp.new(s1), s2)
     end
   end
 end
